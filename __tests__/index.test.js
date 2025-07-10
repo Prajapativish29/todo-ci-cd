1fd4a12d57e72f70b42b5d15a7cc811f8c212940
Prajapativish29 <prajapativish29@gmail.com>
2025-07-10 13:34:15 +0530

    configure jest and added basic passing test

---
+++ __tests__/index.test.js
@@ -0,0 +1,9 @@
+describe('Basic Test', () => {
+  test('should return true', () => {
+    expect(true).toBe(true);
+  });
+
+  test('1 + 1 should equal 2', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
